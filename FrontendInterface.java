c9be83ac09c1be70c6e952cb804236e3ab9f606e
David Cue <dcue@wisc.edu>
2023-03-25 19:13:19 +0000

    Added FrontendInterface file

---
+++ FrontendInterface.java
@@ -0,0 +1,12 @@
+
+public interface FrontendInterface{
+//private MovieRankingInterface movieList;
+public void runCommandLoop();
+public String menu();
+public void loadDataFD();
+public void deleteMovieFD();
+public void addMovieFD();
+public void searchMovieFD();
+public void printListFD();
+public void clearFD();
+}
