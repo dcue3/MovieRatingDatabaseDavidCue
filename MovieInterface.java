1d041f775c7f559c5d5c88b0400e05f44fb80848
Asish <das38@wisc.edu>
2023-03-25 20:35:25 +0000

    Interfaces for DataWrangler added

---
+++ MovieInterface.java
@@ -0,0 +1,11 @@
+
+public interface MovieInterface {
+	// public MovieInterface(String title, int released_year, int runtime, Double rating, String directorName);
+    //methods
+    public String getTitle();
+    public int getReleasedYear();
+    public int getRuntime();
+    public Double getRating();
+    public String getDirectorName();
+    
+}
---
+++ MovieReaderInterface.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public interface MovieReaderInterface {
+	
+	// public MovieReaderInterface();
+	// the only method
+    public List<MovieInterface> readMoviesFromFile (String filename) throws FileNotFoundException;
+
+}
