a79e10a3b2d33fd2786b39048ff6c34bdd7bee5f
Asish <das38@wisc.edu>
2023-03-27 04:13:13 +0000

    Junit tests implemented

--- DataWranglerTests.java
+++ DataWranglerTests.java
@@ -1,17 +1,73 @@
 import java.io.FileNotFoundException;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import java.util.List;
 
 public class DataWranglerTests {
-	public static void main(String[] args) {
-		MovieReaderInterface some = new MovieReaderDW();
+	
+	/**
+	 * Junit test to test the readMoviesFromFile of the MoviesReaderDW class.
+	 * Also test for getTitle() and getRating() method in the MovieDW class
+	 */
+	@Test
+	public void test1() {
+		List<MovieInterface> movies = null;
+		MovieReaderInterface movie_reader = new MovieReaderDW();
 		try {
-			List<MovieInterface> posts =some.readMoviesFromFile("csv.txt");
-			System.out.println("A");
-			System.out.println(posts.get(0).getTitle());
+			movies = movie_reader.readMoviesFromFile("./data/test.txt");
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+		// checking if the readMoviesFromFile read data from the entire test.txt file
+		assertEquals(movies.size(), 1000); // there are 1000 movies in the dataset
+		// the first movie in the dataset is The Shawshank Redemption"
+		assertEquals(movies.get(0).getTitle(), "The Shawshank Redemption");
+		//checking the rating of the first movie
+		assertEquals(movies.get(0).getRating(), Double.valueOf(9.3));
+	}
+	
+	/**
+	 * Junit test for checking the correctness of the getting runtime for each movies
+	 * since the runtime was in the format of (time min)
+	 * Also testing the .getRuntime() method of MovieDW class.
+	 */
+	@Test
+	public void test2() {
+		List<MovieInterface> movies = null;
+		MovieReaderInterface movie_reader = new MovieReaderDW();
+		
+		try {
+			movies = movie_reader.readMoviesFromFile("./data/test.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		assertEquals(movies.get(1).getRuntime(), 175);
+		assertEquals(movies.get(2).getRuntime(), 152);
+		assertEquals(movies.get(50).getRuntime(), 102);
+	}
+	
+	/**
+	 * JUnit test for testing the two compareTo methods of MovieDW class
+	 */
+	@Test
+	public void test3() {
+		//comparing just with double value of rating
+		List<MovieInterface> movies = null;
+		MovieReaderInterface movie_reader = new MovieReaderDW();
+		
+		try {
+			movies = movie_reader.readMoviesFromFile("./data/test.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		// because we know the rating of the first movie is greater than 5
+		assertEquals(movies.get(0).compareTo(5.0), 1);
+		// comparing the objects based on ratings.
+		// creating a movie object
+		MovieInterface testMovie = new MovieDW("some title", 2023, 120, 8.5, "Christopher Nolan");
+		//comparing this with the first movie should return -1
+		assertEquals(testMovie.compareTo(movies.get(0)), -1); // because its rating is lower.
+		//comparing with Terminator 2: Judgment Day should return 0 because its rating is also 8.5
+		assertEquals(testMovie.compareTo(movies.get(44)), 0);
+	}
 }
-
--- MovieReaderDW.java
+++ MovieReaderDW.java
@@ -5,10 +5,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MovieReaderDW implements MovieReaderInterface {
+        
 
+	/**
+	 * Method for creating movie objects from csv file which contains movie interfaces.
+	 *@Param- filename- the name of the file to be parsed.
+	 */
 	@Override
 	public List<MovieInterface> readMoviesFromFile(String filename) throws FileNotFoundException {
-		// TODO Auto-generated method stub
+		//creating a list to store the movie objects.
 		ArrayList<MovieInterface> movies = new ArrayList<>();
 		Scanner in = new Scanner(new File(filename));
 		while (in.hasNextLine()) {
@@ -17,11 +22,12 @@ public class MovieReaderDW implements MovieReaderInterface {
 			// getting all the parts
 			//counting even number of quotes
 			String[] parts = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-			System.out.println(parts[0]);
+			//creating and adding new movie objects into the list
 			movies.add(new MovieDW(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2].split(" ")[0]), Double.valueOf(parts[3]), parts[4]));
 		}
+		//closing the scanner
 		in.close();
-		return movies;
+		return movies;//returning the list of Movie objects.
 	}
 }
 
