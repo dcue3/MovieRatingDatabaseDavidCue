3bb2773c880f82c02f12411d6e7fa5c83218021e
Asish <das38@wisc.edu>
2023-04-01 20:14:24 +0000

    Revert "data wrangler merged"
    This reverts commit b7ba547cd86c35cbfe66f400dced97bd475f0c8e, reversing
    changes made to e1bebbc45fda52c4a60bb1955fb436487b2fd43f.

--- MovieInterface.java
+++ MovieInterface.java
@@ -1,11 +1,10 @@
-
-public interface MovieInterface {
+public interface MovieInterface extends Comparable<MovieInterface> {
 // public MovieInterface(String title, int released_year, int runtime, Double rating, String directorName);
     public String getTitle();
     public int getReleasedYear();
     public int getRuntime();
     public Double getRating();
     public String getDirectorName();
-    public int compareTo(MovieInterface movie);
-    public int compareTo(double rating);
+    public int compareTo(MovieInterface y);
+    public int compareTo(double x);
 }
